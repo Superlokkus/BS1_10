770f5265f9fae65c6b3b70e982251f8670a2d08e
Markus Klemm <markus@markusklemm.net>
2014-02-01 12:13:41 +0100

    Initial Commit

---
+++ BS_Prak_10_Signale/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  BS_Prak_10_Signale
+//
+//  Created by Markus Klemm on 01.02.14.
+//  Copyright (c) 2014 net.markusklemm. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
