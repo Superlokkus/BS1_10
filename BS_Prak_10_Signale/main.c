1e6007b25e9ac0d9ab5ec724a0f35eb843598dbe
Markus Klemm <markus@markusklemm.net>
2014-02-03 14:29:38 +0100

    Aufgabe 2 fertig

--- BS_Prak_10_Signale/main.c
+++ BS_Prak_10_Signale/main.c
@@ -7,12 +7,34 @@
 //
 
 #include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+void sigusr1Hdl(int sig)
+{
+    printf("SIGUSR1 empfangen!\n");
+    signal(SIGUSR1, SIG_DFL);
+}
 
 int main(int argc, const char * argv[])
 {
+    
+    if (signal(SIGUSR1, sigusr1Hdl) == SIG_ERR)
+    {
+        perror("This should never happen (Signal(SIGUSR1,) failed");
+    }
+    
+    if (kill(getpid(), SIGUSR1)) {
+        perror("First kill() failed");
+    }
+    
+    if (kill(getpid(), SIGUSR1)) {
+        perror("Second kill() failed");
+    }
+    
+    
+    
 
-    // insert code here...
-    printf("Hello, World!\n");
     return 0;
 }
 
